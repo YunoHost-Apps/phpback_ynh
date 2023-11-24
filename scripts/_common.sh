4d5adbb0a411e71bd80c24a4e4347f47c3fe27f2
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-11-24 14:20:53 +0100

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,9 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.4"
+#REMOVEME? YNH_PHP_VERSION="7.4"
 
-pkg_dependencies="php${YNH_PHP_VERSION}-mysql"
+#REMOVEME? pkg_dependencies="php${YNH_PHP_VERSION}-mysql"
 
 #=================================================
 # PERSONAL HELPERS
